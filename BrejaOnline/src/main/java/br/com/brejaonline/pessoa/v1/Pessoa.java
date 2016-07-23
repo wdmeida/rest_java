ff225c656ee26e49008b9d2cb1f1c72a0c6874e2
Wagner Almeida <wdmeida@gmail.com>
2016-07-23 18:45:13 -0300

    Criação do projeto de exemplo utilizando maven

---
+++ BrejaOnline/src/main/java/br/com/brejaonline/pessoa/v1/Pessoa.java
@@ -0,0 +1,101 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.8-b130911.1802 
+// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2016.07.23 às 06:41:47 PM BRT 
+//
+
+
+package br.com.brejaonline.pessoa.v1;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java de Pessoa complex type.
+ * 
+ * <p>O seguinte fragmento do esquema especifica o conteúdo esperado contido dentro desta classe.
+ * 
+ * <pre>
+ * &lt;complexType name="Pessoa">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="nome" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *       &lt;attribute name="id" type="{http://www.w3.org/2001/XMLSchema}long" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Pessoa", propOrder = {
+    "nome"
+})
+@XmlSeeAlso({
+    PessoaFisica.class
+})
+public abstract class Pessoa {
+
+    @XmlElement(required = true)
+    protected String nome;
+    @XmlAttribute(name = "id")
+    protected Long id;
+
+    /**
+     * Obtém o valor da propriedade nome.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * Define o valor da propriedade nome.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNome(String value) {
+        this.nome = value;
+    }
+
+    /**
+     * Obtém o valor da propriedade id.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Long }
+     *     
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Define o valor da propriedade id.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Long }
+     *     
+     */
+    public void setId(Long value) {
+        this.id = value;
+    }
+
+}
---
+++ BrejaOnline/src/main/java/br/com/brejaonline/pessoa/v1/PessoaFisica.java
@@ -0,0 +1,71 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.8-b130911.1802 
+// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2016.07.23 às 06:41:47 PM BRT 
+//
+
+
+package br.com.brejaonline.pessoa.v1;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java de PessoaFisica complex type.
+ * 
+ * <p>O seguinte fragmento do esquema especifica o conteúdo esperado contido dentro desta classe.
+ * 
+ * <pre>
+ * &lt;complexType name="PessoaFisica">
+ *   &lt;complexContent>
+ *     &lt;extension base="{http://brejaonline.com.br/pessoa/v1}Pessoa">
+ *       &lt;sequence>
+ *         &lt;element name="cpf" type="{http://brejaonline.com.br/pessoa/v1}CPF"/>
+ *       &lt;/sequence>
+ *     &lt;/extension>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "PessoaFisica", propOrder = {
+    "cpf"
+})
+public class PessoaFisica
+    extends Pessoa
+{
+
+    @XmlElement(required = true)
+    protected String cpf;
+
+    /**
+     * Obtém o valor da propriedade cpf.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * Define o valor da propriedade cpf.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCpf(String value) {
+        this.cpf = value;
+    }
+
+}
